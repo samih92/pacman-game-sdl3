b7b4e9624feabfba62674297b8df5eaa72d0e3fc
samiho_92@hotmail.com <samiho_92@hotmail.com>
2025-09-05 00:08:19 -0400

    adding SDL window

--- Pacman_game_SDL3/main.cpp
+++ Pacman_game_SDL3/main.cpp
@@ -13,8 +13,25 @@ int main() {
 		return 1;
 	}
 
-	SDL_Window *window=SDL_CreateWindow()
+	SDL_Window* window = SDL_CreateWindow("Pacman game", SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_MINIMIZED);
 
+	if (window == NULL) {
+		SDL_Quit();
+		return 2;
+	}
+
+	bool quit = false;
+	SDL_Event event;
+
+	while (!quit) {
+		while (SDL_PollEvent(&event)) {
+			if (event.type == SDL_EVENT_QUIT) {
+				quit = true;
+			}
+		}
+	}
+
+	SDL_DestroyWindow(window);
 	SDL_Quit();
 
 	return 0;
